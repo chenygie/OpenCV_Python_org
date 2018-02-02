2592fe541d34e451d94a35aea649d9ad61fb0355
chen <chenyigenalex@gmail.com>
2018-02-02 11:29:46 +0800

    添加抓取视频和保存，最后提交版本

--- opencv_org.py
+++ opencv_org.py
@@ -49,30 +49,33 @@
 #         break
 # cap.release()
 # cv.destroyAllWindows()
+#
+# import numpy as np
+# import cv2 as cv
+#
+# cap = cv.VideoCapture(0)
+# fourcc = cv.VideoWriter_fourcc(*"XVID")
+# size = (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
+# fps = 20.0
+# out = cv.VideoWriter("output1.mov",fourcc,fps,size)
+# cv.namedWindow("frame",cv.WINDOW_NORMAL)
+# while(cap.isOpened()):
+#     ret,frame = cap.read()
+#     if ret == True:
+#         # 对获取的视频进行反转
+#         # flip(src_img, des_img, 1); // 1
+#         # 代表水平方向旋转180度
+#         # // flip(src_img, des_img, 0); // 0
+#         # 代表垂直方向旋转180度
+#         # // flip(src_img, des_img, -1); // -1
+#         # 代表垂直和水平方向同时旋转
+#         frame = cv.flip(frame,180)
+#         cv.imshow("frame",frame)
+#         out.write(frame)
+#         if cv.waitKey(1) & 0xff == ord('q'):
+#             break
+# cap.release()
+# out.release()
+# cv.destroyAllWindows()
 
-import numpy as np
-import cv2 as cv
 
-cap = cv.VideoCapture(0)
-fourcc = cv.VideoWriter_fourcc(*"XVID")
-size = (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
-fps = 20.0
-out = cv.VideoWriter("output1.mov",fourcc,fps,size)
-while(cap.isOpened()):
-    ret,frame = cap.read()
-    if ret == True:
-        # 对获取的视频进行反转
-        # flip(src_img, des_img, 1); // 1
-        # 代表水平方向旋转180度
-        # // flip(src_img, des_img, 0); // 0
-        # 代表垂直方向旋转180度
-        # // flip(src_img, des_img, -1); // -1
-        # 代表垂直和水平方向同时旋转
-        frame = cv.flip(frame,1)
-        cv.imshow("frame",frame)
-        out.write(frame)
-        if cv.waitKey(1) & 0xff == ord('q'):
-            break
-cap.release()
-out.release()
-cv.destroyAllWindows()
