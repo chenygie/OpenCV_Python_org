72a38eb5a4a3e8495ee7c267caf22e30cb571c8e
chen <chenyigenalex@gmail.com>
2018-02-02 16:47:41 +0800

    添加鼠标响应事件

--- draw_opencv.py
+++ draw_opencv.py
@@ -56,3 +56,64 @@
 #         break
 # cv.destroyAllWindows()
 
+# '''
+# 查看opencv 中所有的EVENT 事件
+# '''
+# import cv2 as cv
+# event = [i for i in dir(cv) if 'EVENT' in i ]
+# print("opencv 中EVENT：",event)
+
+
+'''根据我
+们选择的模式在拖动鼠标时绘制矩形或者是圆圈（就像画图程序中一样）。所以
+我们的回调函数包含两部分，一部分画矩形，一部分画圆圈。这是一个典型的
+例子他可以帮助我们更好理解与构建人机交互式程序，比如物体跟踪，图像分
+割等。
+'''
+
+import cv2 as cv
+import numpy as np
+
+# 当鼠标按下时候为True
+drawing = False
+# 如果mode 为true 绘制矩形。按下'm' 变成绘制曲线。
+
+mode = True
+ix,iy = -1,-1
+
+# 创建回调函数
+def draw_circle(event,x,y,flags,param):
+    global ix,iy,drawing,mode
+#     当按下左键是返回起始位置坐标
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+#       当鼠标左键按下并移动是绘图图形。event 可以查看移动，flag 查看是否按下
+    elif event==cv.EVENT_MOUSEMOVE and flags==cv.EVENT_FLAG_LBUTTON:
+        if drawing == True:
+            if mode == True:
+                cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            else:
+#                  绘制圆圈，小圆点连在一起就成了线，
+                cv.circle(img,(x,y),radius=50,color=(0,0,255))
+#               下面注释掉的代码是起始点为圆心，起点到终点为半径的
+#                r=int(np.sqrt((x-ix)**2+(y-iy)**2))
+#                cv2.circle(img,(x,y),r,(0,0,255),-1)
+# 当鼠标松开停止绘画。
+    elif event==cv.EVENT_LBUTTONUP:
+        drawing==False
+#       if mode==True:
+#           cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+#       else:
+#           cv2.circle(img,(x,y),5,(0,0,255),-1)
+img = np.zeros((1024,1024,3),np.uint8)
+cv.namedWindow("image",cv.WINDOW_NORMAL)
+cv.setMouseCallback("image",draw_circle)
+
+while(True):
+    cv.imshow('image',img)
+    k = cv.waitKey(1) & 0xff
+    if k==ord('m'):
+        mode = not mode
+    elif k==ord('q'):
+        break
